431ce4eed91978a408fb7b3aa6ac35c29952ce9e
Amit Chaudhary <studenton.com@gmail.com>
2017-03-07 08:04:38 +0545

    Refactor injector

--- scripts/stackoverflow/inject.js
+++ scripts/stackoverflow/inject.js
@@ -3,8 +3,6 @@
 Array.from(document.getElementsByTagName('pre')) // get all code snippets
 .forEach(function (block) {
 	block.addEventListener('dblclick', function (event) {
-		event.preventDefault();
-		event.stopPropagation();
 		
 		// Add snippet to range
 		var range = document.createRange();
