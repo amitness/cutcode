5fe1b595006854d8d32fef2b802d283faa4efab3
Will Biddy <willbiddy@gmail.com>
2017-03-22 11:28:59 -0400

    Improving layout of options page

--- scripts/getHotkeys.js
+++
@@ -1,8 +0,0 @@
-// linking to Mousetrap js
-var imported = document.createElement('script');
-imported.src = 'mousetrap.js';
-document.head.appendChild(imported);
-
- Mousetrap.bind('5', function() {
-	 alert("5");
- });
--- scripts/stackoverflow/inject.js
+++ scripts/stackoverflow/inject.js
@@ -1,21 +1,76 @@
 'use strict';
+var color = '',
+attribution = false,
+commenting = '',
+codeAttribution = '',
+hotkey = '';
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+	if (item.color){ //is color set
+		color = item.color;
+	}
+	else{
+		color = '#0D0';
+	}
+	if (item.attribution){ //is attribution checked
+		attribution = true;
+		if (item.commenting){ //is commenting set
+			commenting = item.commenting;
+		}
+	}
+}
+function isNotChrome(){
+	return typeof browser !== typeof undefined ? true : false;
+}
+
+ 
+ 
+if (isNotChrome){
+	chrome.storage.local.get(null, onGot);
+}
+else{
+	var getting = browser.storage.local.get();
+	getting.then(onGot, onError);
+}
+
+
 
 Array.from(document.getElementsByTagName('pre')) // get all code snippets
 .forEach(function (block) {
-	
 	block.addEventListener('dblclick', function (event) {
 		// Reference: http://stackoverflow.com/a/6462980/3485241
+		console.log(commenting);
+		//gets the address of the current page
+		if (attribution){
+			codeAttribution = commenting + ' source: ' + window.location.toString()+'<br/>';
+		}
+		var copyText = block.innerHTML;
+		
+		copyText = codeAttribution+ copyText;
+		//copy old HTML
+		var oldHTML = block.innerHTML;
+		
+		//replace with new HTML
+		block.innerHTML = copyText;
 		
 		// Add snippet to range
 		var range = document.createRange();
 		range.selectNode(block);
-
+		
+		
 		// Copy snippet to clipboard
 		try {
 			window.getSelection().addRange(range);
 			document.execCommand('copy');
 			window.getSelection().removeAllRanges();
-			block.style.border = '2px solid #0D0';
+			
+			//replace new with old HTML
+			block.innerHTML = oldHTML;
+			block.style.border = '2px solid '+color;
 			setTimeout(function () {
 			  return block.style.border = 'none';
 			}, 500);
@@ -23,5 +78,4 @@ Array.from(document.getElementsByTagName('pre')) // get all code snippets
 			console.log('Failed to copy', err);
 		}
 	});
-
 });
