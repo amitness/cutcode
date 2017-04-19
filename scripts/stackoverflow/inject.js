a312cde05e8a31acfcadf18f20164ea1f6216554
Andrew <andrewmbyrd@gmail.com>
2017-04-19 17:49:44 -0500

    Snippets and options are correctly configured to show up

--- scripts/stackoverflow/inject.js
+++ scripts/stackoverflow/inject.js
@@ -9,12 +9,22 @@ Array.from(document.getElementsByTagName('pre')) // get all code snippets
 		// Add snippet to range
 		var range = document.createRange();
 		range.selectNode(block);
-		console.log(range.extractContents());
+		
 
 		// Copy snippet to clipboard
 		try {
 			window.getSelection().addRange(range);
 			document.execCommand('copy');
+			chrome.storage.sync.get("snippetHistory", function(result){
+				console.log(result.snippetHistory);
+				result.snippetHistory.unshift({
+					snippet: range.toString(),
+				  URI: range.commonAncestorContainer.baseURI,
+					date: Date.now()
+				});
+				chrome.storage.sync.set({snippetHistory: result.snippetHistory});
+			});
+
 			window.getSelection().removeAllRanges();
 			block.style.outline = '2px solid #0D0';
 			setTimeout(function () {
